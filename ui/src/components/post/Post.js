836175eb44530f136ff12fd30cb394135243f726
jrryrig <49033422+jrryrig@users.noreply.github.com>
2021-11-05 22:24:00 -0400

    Profile Page

--- ui/src/components/post/Post.js
+++ ui/src/components/post/Post.js
@@ -1,9 +1,18 @@
 import { MoreVert } from '@material-ui/icons';
+import { useState } from 'react';
 import { Users } from '../../dummyData';
 
 import './post.css';
 
 export default function Post({ post }) {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="post">
       <div className="postWrapper">
@@ -29,9 +38,19 @@ export default function Post({ post }) {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="/assets/like.png" alt="" className="likeIcon" />
-            <img src="/assets/heart.png" alt="" className="likeIcon" />
-            <span className="postLikeCounter">{post.like}</span>
+            <img
+              className="likeIcon"
+              src="/assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src="/assets/heart.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="postLikeCounter">{like}</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
